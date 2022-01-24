86ffde9482406c8fbe4b73a1f08673513ae22fba
teredet <artem.vanzytler@gmail.com>
2022-01-24 13:52:23 +0200

    Add Post model

--- base/models.py
+++ base/models.py
@@ -1,5 +1,21 @@
+from statistics import mode
+from xml.parsers.expat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from matplotlib.pyplot import cla
 
 class CustomUser(AbstractUser):
     pass
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    body = models.TextField(null=True, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
