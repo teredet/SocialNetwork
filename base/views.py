83090f6eb2945de7eef154f3c1a9b82130d1e4af
teredet <artem.vanzytler@gmail.com>
2022-01-24 15:21:44 +0200

    Delete trash

--- base/views.py
+++ base/views.py
@@ -33,42 +33,6 @@ class LoginView(View):
         return render(request, self.template_name, {'form': form})
 
 
-
-    # def post(self, request):
-    #     username = request.POST['username']
-    #     password = request.POST['password']
-    #     user = authenticate(username=username, password=password)
-
-    #     if user is not None:
-    #         if user.is_active:
-    #             login(request, user)
-
-    #             return HttpResponseRedirect('/form')
-    #         else:
-    #             return HttpResponse("Inactive user.")
-    #     else:
-    #         return HttpResponseRedirect('login')
-
-    #     return render(request, "index.html")
-
-    # def get(self, request, *args, **kwargs):
-    #     form = CustomUserChangeForm(request.POST or None)
-    #     return render(request, 'base/login.html', {'form': form})
-
-
-    # def post(self, request, *args, **kwargs):
-    #     form = CustomUserChangeForm(request.POST or None)
-    #     if form.is_valid():
-    #         username = form.cleaned_data['username']
-    #         password = form.cleaned_data['password']
-    #         user = authenticate(username=username, password=password)
-    #         if user:
-    #             login(request, user)
-    #             return HttpResponseRedirect('/')
-    #     context = {'form':form,}
-    #     return render(request, 'base/login.html', {'form': form})
-
-
 class RegistrationView(CreateView):
     template_name = 'base/registration.html'
     success_url = reverse_lazy('login')
